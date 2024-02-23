0064b2eb4d67ac30f1c4e10a727374997acf97e5
shweta-rc13 <shwetachaurasiya1313@gmail.com>
2024-02-23 21:17:36 +0530

    API

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const API_KEY = "b559b51a532b438f81904ac55513e198";
 const url = "https://newsapi.org/v2/everything?q=";
 
-window.addEventListener('load',() => fetchNews())
+window.addEventListener('load',() => fetchNews("India"));
+
+async function fetchNews (query){
+    fetch('${url}${query}&apiKey=${API_KEY}');
+}
